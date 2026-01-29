133f2b96872eaa8a650bcdcaf73912e100bf43b8
SofiaKubo <skuboreva@gmail.com>
2026-01-29 16:10:02 +0100

    feat(theme): add ThemeProvider and enable theme switching in Storybook

---
+++ .storybook/preview.tsx
@@ -0,0 +1,25 @@
+import type { Preview } from '@storybook/react';
+import { ThemeProvider } from '../src/app/providers/ThemeProvider';
+import '../src/app/index.css';
+
+const preview: Preview = {
+  decorators: [
+    (Story) => (
+      <ThemeProvider>
+        <Story />
+      </ThemeProvider>
+    ),
+  ],
+
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+};
+
+export default preview;
