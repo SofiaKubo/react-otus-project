8d3246e28c2661ecc9a5955fa402739d5ff23cb9
SofiaKubo <skuboreva@gmail.com>
2026-01-29 16:25:02 +0100

    feat(storybook): add global theme toolbar for all stories

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -3,23 +3,46 @@ import { ThemeProvider } from '../src/app/providers/ThemeProvider';
 import '../src/app/index.css';
 
 const preview: Preview = {
-  decorators: [
-    (Story) => (
-      <ThemeProvider>
-        <Story />
-      </ThemeProvider>
-    ),
-  ],
-
   parameters: {
-    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
-        date: /Date$/,
+        date: /Date$/i,
       },
     },
   },
+
+  globalTypes: {
+    theme: {
+      description: 'Global theme for components',
+      defaultValue: 'light',
+      toolbar: {
+        title: 'Theme',
+        icon: 'circlehollow',
+        items: [
+          { value: 'light', icon: 'sun', title: 'Light' },
+          { value: 'dark', icon: 'moon', title: 'Dark' },
+        ],
+        dynamicTitle: true,
+      },
+    },
+  },
+
+  decorators: [
+    (Story, context) => {
+      const theme = context.globals.theme || 'light';
+
+      if (typeof document !== 'undefined') {
+        document.body.className = `theme-${theme}`;
+      }
+
+      return (
+        <ThemeProvider>
+          <Story />
+        </ThemeProvider>
+      );
+    },
+  ],
 };
 
 export default preview;
