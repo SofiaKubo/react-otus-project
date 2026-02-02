1288e62176d3b187fbf21429f2833718d159c2b2
SofiaKubo <skuboreva@gmail.com>
2026-02-02 12:33:53 +0100

    chore: add LanguageProvider barrel exports

---
+++ src/app/providers/LanguageProvider/index.ts
@@ -0,0 +1,3 @@
+export { LanguageProvider } from './LanguageProvider';
+export { useLanguage, LanguageContext } from './LanguageContext';
+export type { Language, LanguageContextValue } from './LanguageContext';
