51c91f11d5bf79aa961daba2be05ca5dc8715c74
SofiaKubo <skuboreva@gmail.com>
2026-02-02 12:37:39 +0100

    chore: wire LanguageProvider to app tree

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,15 @@ import './shared/i18n/i18n';
 import './app/index.css';
 import App from './app/App';
 import { ThemeProvider } from './app/providers/ThemeProvider';
+import { LanguageProvider } from './app/providers/LanguageProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <ThemeProvider>
-      <App />
+      <LanguageProvider>
+        <App />
+      </LanguageProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
